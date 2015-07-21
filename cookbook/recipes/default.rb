77625aa1294217486e2e71d06d03e340139d5d69
jmanero <john.manero@gmail.com>
2015-07-21 01:06:54 -0400

    Checkout code from git

--- cookbook/recipes/default.rb
+++ cookbook/recipes/default.rb
@@ -2,13 +2,32 @@
 # Cookbook Name:: unbound-build
 # Recipe:: default
 #
-# Copyright (C) 2015 John Manero
+# The MIT License (MIT)
 #
-# TODO: MIT License
+# Copyright (c) 2015 John Manero <john.manero@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 #
 include_recipe 'apt::default'
 include_recipe 'build-essential::default'
 
+package 'gdb'
 package 'libtool'
 package 'pkg-config'
 
@@ -17,6 +36,7 @@ package 'libexpat1-dev'
 package 'libssl-dev'
 package 'protobuf-c-compiler'
 
+package 'git'
 package 'nodejs'
 
 user node['unbound-build']['user'] do
@@ -29,37 +49,24 @@ user 'unbound' do
   shell '/usr/sbin/nologin'
 end
 
-directory node['unbound-build']['root'] do
+directory '/usr/local/etc/unbound' do
+  recursive true
+  owner 'unbound'
+  group 'unbound'
+end
+
+template '/usr/local/etc/unbound/unbound.conf' do
+  source 'unbound.conf.erb'
+end
+
+directory node['unbound-build']['dir'] do
   owner node['unbound-build']['user']
   group node['unbound-build']['user']
 end
 
-## Fetch the sources to build
-remote_file 'unbound-1.5.3' do
-  source node['unbound-build']['source']['unbound']
-  path ::File.join(node['unbound-build']['root'], 'unbound-1.5.3.tar.gz')
-  owner node['unbound-build']['user']
+git node['unbound-build']['dir'] do
+  repository node['unbound-build']['repository']
+  reference node['unbound-build']['reference']
+  user node['unbound-build']['user']
   group node['unbound-build']['user']
 end
-
-remote_file 'fstrm-0.2.0' do
-  source node['unbound-build']['source']['fstrm']
-  path ::File.join(node['unbound-build']['root'], 'fstrm-0.2.0.tar.gz')
-  owner node['unbound-build']['user']
-  group node['unbound-build']['user']
-end
-
-## Unpack and build 'em
-unarchive 'unbound-1.5.3' do
-  source ::File.join(node['unbound-build']['root'], 'unbound-1.5.3.tar.gz')
-  destination ::File.join(node['unbound-build']['root'], 'unbound')
-  owner node['unbound-build']['user']
-  group node['unbound-build']['user']
-end
-
-# unarchive 'fstrm-0.2.0' do
-#   source ::File.join(node['unbound-build']['root'], 'fstrm-0.2.0.tar.gz')
-#   destination ::File.join(node['unbound-build']['root'], 'fstrm')
-#   owner node['unbound-build']['user']
-#   group node['unbound-build']['user']
-# end
